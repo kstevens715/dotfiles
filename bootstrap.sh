204c311cd96512868c13a1be5ecb14e9836d38b3
Kyle Stevens <kstevens715@gmail.com>
2014-05-23 13:06:31 -0400

    No prompt and fix package name

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,34 +8,36 @@ echo 'Bootstrapping system'
 sudo apt-get update && sudo apt-get -y upgrade
 
 # Add repos
-sudo apt-add-repository ppa:system76-dev/stable
-sudo apt-add-repository ppa:webupd8team/java
-sudo apt-add-repository ppa:nviennot/tmate
-sudo apt-add-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
+sudo apt-add-repository ppa:system76-dev/stable -y
+sudo apt-add-repository ppa:webupd8team/java -y
+sudo apt-add-repository ppa:nviennot/tmate -y
+sudo apt-add-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner" -y
 
 # Update package list
 sudo apt-get update
 
-sudo apt-get install  aptitude \
-                      bundler \
-                      curl \
-                      freetds-dev \
-                      git \
-                      kmymoney \
-                      libpq-dev \
-                      libxlst-dev \
-                      oracle-java8-installer \
-                      phantomjs \
-                      postgresql \
-                      python-software-properties \
-                      rabbitmq-server \
-                      rake \
-                      redis-server \
-                      skype \
-                      tmate \
-                      tmux \
-                      unixodbc-dev \
-                      xchat
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt-get -q -y install  aptitude \
+                            bundler \
+                            curl \
+                            freetds-dev \
+                            git \
+                            kmymoney \
+                            libpq-dev \
+                            libxlst1-dev \
+                            oracle-java8-installer \
+                            phantomjs \
+                            postgresql \
+                            python-software-properties \
+                            rabbitmq-server \
+                            rake \
+                            redis-server \
+                            skype \
+                            tmate \
+                            tmux \
+                            unixodbc-dev \
+                            xchat
 
 #TODO: clone dot-files and get that to take over the process. Maybe only install minimal packages above and let the Rakefile
 # take care of the test.
