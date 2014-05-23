6ad68f75573ea67bde3d850fb9e46d107c4160fc
Kyle Stevens <kstevens715@gmail.com>
2014-05-23 12:34:28 -0400

    Add required repositories

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,17 +5,16 @@
 
 echo 'Bootstrapping system'
 # Install updates
+sudo apt-get update && sudo apt-get -y upgrade
+
+# Add repos
+sudo apt-add-repository ppa:system76-dev/stable
+sudo apt-add-repository ppa:webupd8team/java
+sudo apt-add-repository ppa:nviennot/tmate
+sudo apt-add-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
+
+# Update package list
 sudo apt-get update
-sudo apt-get upgrade
-#
-# # Add repos
-# sudo apt-add-repository ppa:system76-dev/stable
-# sudo apt-add-repository ppa:webupd8team/java
-# sudo apt-add-repository ppa:nviennot/tmate
-# sudo apt-add-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
-#
-# # Update package list
-# sudo apt-get update
 #
 # sudo apt-get install curl \
 #          git \
