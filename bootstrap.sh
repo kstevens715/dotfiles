fd3c948f2d8b2ecaadd98acac3676979053134fc
Kyle Stevens <kstevens715@gmail.com>
2014-05-23 12:53:12 -0400

    Install required packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,6 +15,27 @@ sudo apt-add-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -
 
 # Update package list
 sudo apt-get update
-#
-# sudo apt-get install curl \
-#          git \
+
+sudo apt-get install  aptitude \
+                      bundler \
+                      curl \
+                      freetds-dev \
+                      git \
+                      kmymoney \
+                      libpq-dev \
+                      libxlst-dev \
+                      oracle-java8-installer \
+                      phantomjs \
+                      postgresql \
+                      python-software-properties \
+                      rabbitmq-server \
+                      rake \
+                      redis-server \
+                      skype \
+                      tmate \
+                      tmux \
+                      unixodbc-dev \
+                      xchat
+
+#TODO: clone dot-files and get that to take over the process. Maybe only install minimal packages above and let the Rakefile
+# take care of the test.
