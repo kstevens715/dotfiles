e12fa2487390c48bb225d89cc7cc7e8409dfdc35
Kyle Stevens <kstevens715@gmail.com>
2014-05-23 12:23:31 -0400

    Initial test to see if this works how I want

---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# This is a bootstrap script that sets up my Ubuntu system.
+#
+
+echo 'It Works'
+# Install updates
+# sudo apt-get update
+# sudo apt-get upgrade
+#
+# # Add repos
+# sudo apt-add-repository ppa:system76-dev/stable
+# sudo apt-add-repository ppa:webupd8team/java
+# sudo apt-add-repository ppa:nviennot/tmate
+# sudo apt-add-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
+#
+# # Update package list
+# sudo apt-get update
+#
+# sudo apt-get install curl \
+#          git \
