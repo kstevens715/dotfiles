0e03d9fea0ef8896b775c10849d3f7d12f1e6d73
Kyle Stevens <kstevens715@gmail.com>
2014-05-24 09:26:04 -0400

    Minimal necessary bootstrap
    This is an important commit because most of the stuff removed I'm going
    to add back to the rake task after. I wanted the bootstrap script to be
    the minimal necessary to get the dot-files repo functional.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,45 +4,15 @@
 #
 
 echo 'Bootstrapping system'
-# Install updates
+
 sudo apt-get update && sudo apt-get -y upgrade
-
-# Add repos
-sudo apt-add-repository ppa:system76-dev/stable -y
-sudo apt-add-repository ppa:webupd8team/java -y
-sudo apt-add-repository ppa:nviennot/tmate -y
-sudo apt-add-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner" -y
-
-# Update package list
-sudo apt-get update
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt-get -q -y install  aptitude \
-                            bundler \
-                            curl \
-                            freetds-dev \
+sudo apt-get -q -y install  curl \
                             git \
-                            kmymoney \
-                            libpq-dev \
-                            libxslt-dev \
-                            oracle-java8-installer \
-                            phantomjs \
-                            postgresql \
-                            python-software-properties \
-                            rabbitmq-server \
-                            rake \
-                            redis-server \
-                            skype \
-                            tmate \
-                            tmux \
-                            unixodbc-dev \
-                            xchat
-
-#TODO: Install RVM and correct Ruby version
+                            rake
 
 git clone git@github.com:kstevens715/dot-files.git ~/dot-files
 cd ~/dot-files
-rake vim:install
-
-
+rake -T
