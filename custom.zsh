5006f3fc7cd401ee937afe0253923f759f6853d4
Kyle Stevens <kstevens715@gmail.com>
2020-01-21 10:35:09 -0500

    Add zsh config file I can share

---
+++ custom.zsh
@@ -0,0 +1,9 @@
+# ln -s $FILE_PATH $HOME/.oh-my-zsh/lib/
+
+# Variables
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
+# Aliases
+alias cdc="cd ~/code/content_system"
+alias vimcdc="cdc & vim"
+alias vim="nvim"
