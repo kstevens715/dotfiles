e6aaac69083755b34a68fd2efa6a9326e6de7f41
Kyle Stevens <kstevens715@gmail.com>
2020-04-10 11:56:03 -0400

    Add dc alias

--- custom.zsh
+++ custom.zsh
@@ -6,5 +6,6 @@ export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 # Aliases
 alias be="bundle exec"
 alias cdc="cd ~/code/content_system"
+alias dc="docker-compose"
 alias vimcdc="cdc & vim"
 alias vim="nvim"
