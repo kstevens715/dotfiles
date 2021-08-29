d4a9935211c433708baec9d9915239482a16e70c
Kyle Stevens <kyles@kyles-mbp.myfiosgateway.com>
2021-08-29 19:06:58 -0400

    Remove unneeded config

--- custom.zsh
+++ custom.zsh
@@ -4,17 +4,9 @@
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 export SHELL=zsh
 export BAT_THEME="OneHalfDark"
-export SPARK_HOME=/opt/spark
-export PATH=$SPARK_HOME/bin:$PATH
-export PYSPARK_DRIVER_PYTHON=jupyter
-export PYSPARK_DRIVER_PYTHON_OPTS='notebook'
 
 # Aliases
 alias be="bundle exec"
-alias cdc="cd ~/code/content_system"
 alias dc="docker-compose"
-alias vimcdc="cdc & vim"
 alias vim="nvim"
-alias completeprod="rm -f ~/Desktop/_COMPLETE ; touch ~/Desktop/_COMPLETE"
-alias completeqa="rm -f ~/Desktop/_COMPLETE ; echo 'qa' > ~/Desktop/_COMPLETE"
 
