7020482543e5c8763094bd0c9c70207aa4cc4321
Kyle Stevens <kstevens715@gmail.com>
2021-09-13 13:33:02 -0400

    Easier setup

--- install.sh
+++ install.sh
@@ -11,20 +11,14 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 brew install bat firefox mas neovim obsidian postgresql redis slack tmux tmate
 mas install 961632517 # Be Focused Pomodoro Timer
 
-# Install Vim Plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
 cd ~
 
 mkdir .config/nvim/
-ln -s ~/dot-files/init.vim          .config/nvim/
-ln -s ~/dot-files/coc-settings.json .config/nvim/
-ln -s ~/dot-files/custom.zsh        .oh-my-zsh/custom/
-ln -s ~/dot-files/gitconfig         .gitconfig
-ln -s ~/dot-files/tmux.conf         .tmux.conf
-
-nvim -c "PlugInstall" -c "qa"
+ln -s ~/dotfiles/init.vim          .config/nvim/
+ln -s ~/dotfiles/coc-settings.json .config/nvim/
+ln -s ~/dotfiles/custom.zsh        .oh-my-zsh/custom/
+ln -s ~/dotfiles/gitconfig         .gitconfig
+ln -s ~/dotfiles/tmux.conf         .tmux.conf
 
 # Install RVM
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
