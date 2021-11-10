66d08b8cded8c9988d6aad9f4a543516de0997bf
Kyle Stevens <kstevens715@gmail.com>
2021-11-09 20:57:47 -0500

    Change packages

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ curl -sL install-node.now.sh/lts | bash
 
 brew tap heroku/brew
 
-brew install bat firefox git-flow gpg heroku imagemagick mas neovim obsidian postgresql rabbitmq redis rg slack tmux tmate yarn
+brew install bat firefox git-flow gpg heroku imagemagick mas neovim rabbitmq redis rg slack tmux tmate yarn
 
 brew services start postgresql
 brew services start rabbitmq
