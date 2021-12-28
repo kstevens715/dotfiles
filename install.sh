e12fe56a36a69f4e80ccb8b450ac854472a5d968
Kyle Stevens <kstevens715@gmail.com>
2021-12-28 13:19:35 -0500

    Changes

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env zsh
 
-cp ./"Hack Regular Nerd Font Complete.ttf" ~/Library/Fonts/
-
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Install Oh My ZSH
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # Install Node for coc.nvim
 curl -sL install-node.now.sh/lts | bash
 
@@ -21,13 +13,5 @@ brew services start redis
 
 mas install 961632517 # Be Focused Pomodoro Timer
 
-cd ~
-
-mkdir .config/nvim/
-ln -s ~/dotfiles/init.vim          .config/nvim/
 ln -s ~/dotfiles/coc-settings.json .config/nvim/
-ln -s ~/dotfiles/custom.zsh        .oh-my-zsh/custom/
-ln -s ~/dotfiles/gitconfig         .gitconfig
-ln -s ~/dotfiles/tmux.conf         .tmux.conf
 
-echo "Done."
