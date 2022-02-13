a8c185fa31d4d6068fc9212b3ad9073b48ea083a
Kyle Stevens <kstevens715@gmail.com>
2022-02-13 10:22:43 -0500

    Switch to init.lua

---
+++ init.lua
@@ -0,0 +1,31 @@
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+end
+
+vim.cmd [[
+  augroup Packer
+    autocmd!
+    autocmd BufWritePost init.lua PackerCompile
+  augroup end
+]]
+
+local use = require('packer').use
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'EdenEast/nightfox.nvim'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+end)
+
+require('nightfox').load('duskfox')
+
+require('lualine').setup {
+  options = {
+    theme = 'nightfox'
+  },
+}
