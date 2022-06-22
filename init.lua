e2c67f01528b446fe5895714b6d1a9918b25baee
Kyle Stevens <kstevens715@gmail.com>
2022-06-21 20:38:26 -0400

    Show virtual text and underline for diagnostics

--- init.lua
+++ init.lua
@@ -150,9 +150,10 @@ require'nvim-treesitter.configs'.setup {
 
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = false,
     signs = true,
+    underline = true,
     update_in_insert = false,
+    virtual_text = true,
   }
 )
 
