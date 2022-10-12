6f1426a6ae75f18d3931c5ee8d6ceb527798480f
Kyle Stevens <kstevens715@gmail.com>
2022-10-12 15:35:18 -0400

    Add JSON language server

--- init.lua
+++ init.lua
@@ -213,6 +213,7 @@ cmp.setup({
 
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+require'lspconfig'.jsonls.setup{}
 require('lspconfig').solargraph.setup {
   on_attach = on_attach,
   capabilities = capabilities,
