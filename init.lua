ef483a27a2191a90ce4acd5691b73fc152ffb55d
Kyle Stevens <kstevens715@gmail.com>
2023-02-09 11:52:05 -0500

    Don't insert git token if already there
    This makes it so it won't reinsert the token when doing a `git commit
    amend`

--- init.lua
+++ init.lua
@@ -318,12 +318,24 @@ vim.filetype.add({
 function insert_ticket_token()
   local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD")
   local prefix, ticket = string.match(branch, "(%a+)/(%u+%-%d+)")
-    if prefix then
-      local token = string.format("[%s]", ticket)
+  if prefix then
+    local token = string.format("[%s]", ticket)
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+    local found = false
+
+    for _, line in ipairs(lines) do
+      if string.find(line, token, 0, true) then
+        found = true
+        break
+      end
+    end
+
+    if not found then
       vim.api.nvim_buf_set_option(0, "bufhidden", "delete")
       vim.api.nvim_buf_set_lines(0, 1, 1, false, {""})
       vim.api.nvim_buf_set_lines(0, 2, 2, false, {token})
     end
+  end
 end
 
 vim.cmd [[autocmd BufEnter COMMIT_EDITMSG lua insert_ticket_token()]]
