065bf604646d63c8ec2459246d11a63518de01d6
Kyle Stevens <kstevens715@gmail.com>
2024-06-18 10:29:25 -0400

    Revert "Reapply "Reapply "sonokai"""
    This reverts commit c4396de3f60d45c1a27d76d67b161da3e1c6107f.

--- config/nvim/lua/user/colorscheme.lua
+++ config/nvim/lua/user/colorscheme.lua
@@ -1,11 +1,17 @@
 local M = {
-  'sainnhe/sonokai',
+  'mcchrish/zenbones.nvim',
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
+  dependencies = {
+    {
+      'rktjmp/lush.nvim',
+    },
+  },
 }
 
 function M.config()
-  vim.cmd.colorscheme 'sonokai'
+  vim.cmd.colorscheme 'zenbones'
+  vim.o.background = 'light'
 end
 
 return M
