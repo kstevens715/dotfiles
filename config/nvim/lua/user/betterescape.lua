8283efdea25277db88800999e0a7a5cdf9255d35
Kyle Stevens <kstevens715@gmail.com>
2024-10-04 09:31:25 -0400

    Disable better-escape in normal mode
    This also allowed me to unlock the version and use the latest version.
    
    Otherwise, the `j` movement key in visual block selection mode would
    lose the current selection.
    
    See: https://github.com/max397574/better-escape.nvim/issues/102

--- config/nvim/lua/user/betterescape.lua
+++ config/nvim/lua/user/betterescape.lua
@@ -1,10 +1,39 @@
 local M = {
   'max397574/better-escape.nvim',
-  tag = 'v1.0.0',
 }
 
 function M.config()
-  require('better_escape').setup()
+  require('better_escape').setup {
+    mappings = {
+      i = {
+        j = {
+          k = "<Esc>",
+          j = "<Esc>",
+        },
+      },
+      c = {
+        j = {
+          k = "<Esc>",
+          j = "<Esc>",
+        },
+      },
+      t = {
+        j = {
+          k = "<C-\\><C-n>",
+        },
+      },
+      v = {
+        j = {
+          k = false,
+        },
+      },
+      s = {
+        j = {
+          k = "<Esc>",
+        },
+      },
+    }
+  }
 end
 
 return M
