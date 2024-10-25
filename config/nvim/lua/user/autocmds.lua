31dd434ffac8426dee60c80f00ff8858eb9f4160
Kyle Stevens <kstevens715@gmail.com>
2024-10-25 16:23:31 -0400

    Simplify autoformat callback and add json

--- config/nvim/lua/user/autocmds.lua
+++ config/nvim/lua/user/autocmds.lua
@@ -8,23 +8,10 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 })
 
 vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '*.rb',
+  pattern = '*.json,*.rb',
   callback = function(args)
-    local root = vim.fn.getcwd()
-
     if vim.b.disable_autoformat then
       return
-    end
-
-    if vim.fn.filereadable(root .. "/NO_AUTOFORMAT") == 1 then
-      return
-    end
-
-    local excluded_patterns = { "spec%.rb$", "routes.rb$", "application.rb$", "environments", "schema.rb$" }
-    for _, pattern in ipairs(excluded_patterns) do
-      if string.match(args.file, pattern) then
-        return
-      end
     end
 
     vim.lsp.buf.format()
