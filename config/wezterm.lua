af8b0982c457bb3174ba85faa7e958f3344e5e49
Kyle Stevens <kstevens715@gmail.com>
2024-12-25 20:16:27 -0500

    Wezterm

---
+++ config/wezterm.lua
@@ -0,0 +1,23 @@
+local wezterm = require 'wezterm'
+
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Catppuccin Mocha'
+  else
+    return 'Catppuccin Latte'
+  end
+end
+
+return {
+  color_scheme = scheme_for_appearance(get_appearance()),
+  tab_bar_at_bottom = true,
+}
