612415e7b0d2e9a8f5f403063a528107c5b0f4d2
Kyle Stevens <kstevens715@gmail.com>
2024-12-26 10:23:05 -0500

    changes

--- config/nvim/lua/user/copilot.lua
+++ config/nvim/lua/user/copilot.lua
@@ -1,13 +1,5 @@
 return {
   {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    branch = 'main',
-    dependencies = {
-      { 'github/copilot.vim' },
-      { 'nvim-lua/plenary.nvim' },
-    },
-    build = 'make tiktoken',
-    opts = {
-    },
+    'github/copilot.vim',
   },
 }
--- config/nvim/lua/user/lualine.lua
+++ config/nvim/lua/user/lualine.lua
@@ -3,8 +3,26 @@ local M = {
 }
 
 function M.config()
+
+  local function diff_source()
+    local gitsigns = vim.b.gitsigns_status_dict
+    if gitsigns then
+      return {
+        added = gitsigns.added,
+        modified = gitsigns.changed,
+        removed = gitsigns.removed
+      }
+    end
+  end
+
   require('lualine').setup {
     sections = {
+      lualine_b = {
+        {
+          'diff',
+          source = diff_source
+        },
+      },
       lualine_c = {
         {
           'filename',
