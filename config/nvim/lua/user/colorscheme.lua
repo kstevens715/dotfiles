17bd92cda4dd40d1eb584e5ec5f55d9cd2de5835
Kyle Stevens <kstevens715@gmail.com>
2025-02-12 16:37:28 -0500

    nightfox

--- config/nvim/lua/user/colorscheme.lua
+++ config/nvim/lua/user/colorscheme.lua
@@ -1,21 +1,11 @@
 local M = {
-  'loctvl842/monokai-pro.nvim',
+  'EdenEast/nightfox.nvim',
   lazy = false,
   priority = 1000,
 }
 
 function M.config()
-  require('monokai-pro').setup({
-    -- See palette: https://github.com/loctvl842/monokai-pro.nvim?tab=readme-ov-file#palette
-    override = function(c)
-      return {
-          ['@keyword.type'] = { fg = c.base.red, italic = false },
-          ['@keyword.function'] = { fg = c.base.red, italic = false },
-          ['@string.special.symbol'] = { fg = c.base.magenta, italic = false },
-        }
-    end
-  })
-  vim.cmd.colorscheme 'monokai-pro'
+  vim.cmd.colorscheme 'dayfox'
 end
 
 return M
