b7d929af29629b890763960e3da70c3377096e70
Kyle Stevens <kstevens715@gmail.com>
2025-02-19 14:03:47 -0500

    Revert "nightfox"
    This reverts commit ddeb25604bb86e20716e26dba21fe6b941a6b568.

--- config/nvim/lua/user/colorscheme.lua
+++ config/nvim/lua/user/colorscheme.lua
@@ -1,11 +1,21 @@
 local M = {
-  'EdenEast/nightfox.nvim',
+  'loctvl842/monokai-pro.nvim',
   lazy = false,
   priority = 1000,
 }
 
 function M.config()
-  vim.cmd.colorscheme 'dayfox'
+  require('monokai-pro').setup({
+    -- See palette: https://github.com/loctvl842/monokai-pro.nvim?tab=readme-ov-file#palette
+    override = function(c)
+      return {
+          ['@keyword.type'] = { fg = c.base.red, italic = false },
+          ['@keyword.function'] = { fg = c.base.red, italic = false },
+          ['@string.special.symbol'] = { fg = c.base.magenta, italic = false },
+        }
+    end
+  })
+  vim.cmd.colorscheme 'monokai-pro'
 end
 
 return M
