16c1ddf9e472e662df4ef288661cc9414d153a49
Kyle Stevens <kstevens715@gmail.com>
2026-01-08 13:51:56 -0500

    Auto-reload files edited by AI

--- config/nvim/lua/user/autocmds.lua
+++ config/nvim/lua/user/autocmds.lua
@@ -26,3 +26,21 @@ end, {})
 
 vim.cmd 'autocmd BufRead,BufNewFile *.scm set filetype=scheme'
 vim.cmd [[autocmd BufEnter COMMIT_EDITMSG lua require('user.dotfiles').insert_ticket_token()]]
+
+-- Auto-reload files changed outside of Neovim
+vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
+  pattern = '*',
+  callback = function()
+    if vim.fn.getcmdwintype() == '' then
+      vim.cmd('checktime')
+    end
+  end,
+})
+
+-- Poll for changes every 2 seconds (even without focus)
+local autoread_timer = vim.loop.new_timer()
+autoread_timer:start(2000, 2000, vim.schedule_wrap(function()
+  if vim.fn.getcmdwintype() == '' then
+    vim.cmd('checktime')
+  end
+end))
